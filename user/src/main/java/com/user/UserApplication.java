ae61a88edd6b2c5d23946bdc3e0fb914f18ec317
unknown <nishantsayshellotou@gmail.com>
2021-11-22 13:54:27 +0530

    Service resgistration with eureka implementaton

--- user/src/main/java/com/user/UserApplication.java
+++ user/src/main/java/com/user/UserApplication.java
@@ -2,11 +2,12 @@ package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class UserApplication {
 
 	public static void main(String[] args) {
