aaff004e2553c2c44123fcce9137646f751c3275
unknown <nishantsayshellotou@gmail.com>
2021-11-23 11:32:58 +0530

    Basic Project Structure

---
+++ ProductService/src/test/java/com/nish/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nish.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
